fb3d8335b5bb2bffc5a1bfec728dbbf19ec02e1f
HoornbloemCoding <71282824+HoornbloemCoding@users.noreply.github.com>
2020-10-27 20:52:24 +0100

    First real commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var sec = document.getElementById("sec");
+    var top = document.getElementById("top");
+    var navi = document.getElementById("navigation");
+
+    sec.classList.toggle("active");
+    top.classList.toggle("active");
+    navi.classList.toggle("active");
+}
